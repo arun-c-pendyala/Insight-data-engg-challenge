8d91e6c7c6edd01e92766a968ccb15547744c000
Arun Chandra Pendyala <acpendyala@gmail.com>
2017-10-31 09:26:17 -0400

    Update RunningMedian.py

--- src/RunningMedian.py
+++ src/RunningMedian.py
@@ -9,7 +9,7 @@ class RunningMedian:
         self.large = []  # larger half, min heap
     
     def add(self, num): #add number to the right heap
-        if len(self.small) == len(self.large):
+        if len(self.small) == len(self.large): # size of  small is equal to the size of large or lesser than the size of large by 1
             heappush(self.large, -heappushpop(self.small, -num))
         else:
             heappush(self.small, -heappushpop(self.large, num))
@@ -18,10 +18,10 @@ class RunningMedian:
             
             # find the median
     def findMedian(self):
-        if len(self.small) == len(self.large):
+        if len(self.small) == len(self.large): #even number of elements
             return float(self.large[0] - self.small[0]) / 2.0
         else:
-            return float(self.large[0])
+            return float(self.large[0]) #odd number of elements
 
     # find the number of contributions
     def size(self):
